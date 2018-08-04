990a420f25ef64336d74002ac5c8e11ed82c5b33
Andrea Hayes <scoondi@hotmail.com>
2018-08-04 17:17:18 +0200

    Day and night script

---
+++ Tap4Change Game/Assets/Scripts/DayNightDisplayManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightDisplayManager : MonoBehaviour {
+
+int systemHour = System.DateTime.Now.Hour;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	 if(systemHour >= 6 && systemHour <= 17)
+	 {
+    	 Debug.Log("Good Morning/Afternoon!");
+		
+	 }
+
+	 if ((systemHour >= 0 && systemHour <= 5) || (systemHour >= 18)){
+
+		  Debug.Log("Good Evening!");
+
+		  //do stuff with sprite to make it night
+
+	 }
+
+
+
+
+	}
+}
