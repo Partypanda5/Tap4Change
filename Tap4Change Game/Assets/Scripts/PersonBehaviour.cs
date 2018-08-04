93c0cb7fd3a163aa1a5c96043b8ce65e8bfe8c9a
Tiane Erwee <tiane.erwee@gmail.com>
2018-08-04 19:51:47 +0200

    Queue is working

--- Tap4Change Game/Assets/Scripts/PersonBehaviour.cs
+++ Tap4Change Game/Assets/Scripts/PersonBehaviour.cs
@@ -7,44 +7,64 @@ public class PersonBehaviour : MonoBehaviour
 {
 
     private Image myImg;
-	private RectTransform myTransform;
-
+    private RectTransform myTransform;
+    [SerializeField]
     private bool isMoving = false;
+    [SerializeField]
     private bool isLeaving = false;
-    private Vector3 targetLocation;
-    void Start()
+    public Vector3 targetLocation;
+
+    public bool isCollecter = false;
+
+    public void Initialize()
     {
         myImg = this.GetComponent<Image>();
-		myTransform = this.GetComponent<RectTransform>();
-		myTransform.localScale = new Vector3(1, 1, 1);
-        myTransform.localPosition = new Vector3(0, 0, 0);
+        myTransform = this.GetComponent<RectTransform>();
+        myTransform.localScale = new Vector3(1, 1, 1);
+        myTransform.localPosition = new Vector3(-20, 0, 0);
         targetLocation = myTransform.localPosition;
     }
 
     void Update()
     {
-        if (isMoving) {
+        if (isMoving)
+        {
             MoveForward();
         }
-        if (isLeaving) {
-            myTransform.localPosition = Vector3.MoveTowards(myTransform.localPosition, new Vector3(-10, 0, 0), 10);
+        if (isLeaving)
+        {
+            myTransform.localPosition = Vector3.MoveTowards(myTransform.localPosition, targetLocation, 10);
         }
     }
 
-	public void MoveForward() {
-		myTransform.localPosition = Vector3.MoveTowards(myTransform.localPosition, targetLocation, 1);
-        if (Vector3.Distance(myTransform.localPosition, targetLocation) < 0.1f){
+    public void MoveForward()
+    {
+        myTransform.localPosition = Vector3.MoveTowards(myTransform.localPosition, targetLocation, 1);
+        if (Vector3.Distance(myTransform.localPosition, targetLocation) < 0.1f)
+        {
             isMoving = false;
-        } 
-	}
+            if (isCollecter) {
+                PeopleInQueueManager.instance.StartCollecting();
+            }
+        }
+    }
 
-    public void MoveOffScreen() {
+    public void MoveOffScreen()
+    {
+        SetTargetLocation(new Vector3(-100, 0, 0));
         isLeaving = true;
     }
 
-    public void StartMove() {
-    if (myTransform != null)
-        targetLocation = myTransform.localPosition + new Vector3(75, 0, 0);
-        isMoving = true;
+    public void StartMove()
+    {
+        if (myTransform != null)
+        {
+            isMoving = true;
+        }
+    }
+
+    public void SetTargetLocation(Vector3 newTargetLocation)
+    {
+        targetLocation = newTargetLocation;
     }
 }
